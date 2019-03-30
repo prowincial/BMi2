3098e795a27f9f43e4984cafe92959a858fa7390
prowincial <maximelshumak@gmail.com>
2019-03-30 17:28:21 +0100

    wersion 1.0

---
+++ app/src/main/java/com/example/bmi/DataAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.bmi
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class DataAdapter(val users:ArrayList<String>, val wyniks:MutableList<String>,val colors:MutableList<Int>):RecyclerView.Adapter<DataAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View=LayoutInflater.from(parent.context).inflate(R.layout.data_raws,parent,false)
+        return ViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+       return wyniks.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.number.text=users[position]
+        holder.wynik.text=wyniks[position]
+        holder.wynik.setTextColor(colors[position])
+
+
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val number:TextView=itemView.findViewById(R.id.number)
+        val wynik:TextView=itemView.findViewById(R.id.wartosc)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/bmi/HistoryData_activity.kt
@@ -0,0 +1,48 @@
+package com.example.bmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+
+class HistoryData_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_data_activity)
+
+        checkData()
+
+    }
+    private fun checkData(){
+        val names:ArrayList<String> =ArrayList()
+        val wynik:ArrayList<String> = intent.getStringArrayListExtra("wynik")
+        val colors:ArrayList<Int> = intent.getIntegerArrayListExtra("color")
+
+
+
+        val num10:Int
+
+        if (wynik.size < 10){
+            num10 =0
+        }
+        else
+            num10 =wynik.size-10
+
+        val wynik10=wynik.subList(num10,wynik.size)
+        val color10=colors.subList(num10,wynik.size)
+
+        for (i in 1..wynik.size){
+            names.add("$i")
+
+        }
+
+        val recyclerView:RecyclerView=findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = DataAdapter(names,wynik10,color10)
+
+    }
+}
